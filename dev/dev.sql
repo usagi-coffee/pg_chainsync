33ea8bba72b61ccb256835065e1ccea4bfa0caa8
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2024-05-25 13:24:25 +0200

    etc: add erc20 docker container to emulate transfers

--- dev/dev.sql
+++ dev/dev.sql
@@ -7,3 +7,41 @@ $$
 LANGUAGE plpgsql;
 
 SELECT chainsync.add_blocks_job(31337, 'ws://pg-chainsync-foundry:8545', 'custom_block_handler');
+
+CREATE FUNCTION transfer_handler(log chainsync.Log, job_id bigint) RETURNS VOID
+AS $$
+BEGIN
+  RAISE LOG 'Got transfer % from job %', log, job_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE FUNCTION cron_handler(log chainsync.Log, job_id bigint) RETURNS VOID
+AS $$
+BEGIN
+  RAISE LOG 'Got transfer % from cron %', log, job_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+SELECT chainsync.add_events_job(
+  31337,
+  'ws://pg-chainsync-foundry:8545',
+  'transfer_handler',
+  '{
+    "address": "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    "event": "Transfer(address,address,uint256)"
+   }'::JSONB
+);
+
+SELECT chainsync.add_events_cron(
+  31337,
+  'ws://pg-chainsync-foundry:8545',
+  '0 * * * * *',
+  'cron_handler',
+  '{
+    "address": "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    "event": "Transfer(address,address,uint256)",
+    "from_block": 0
+   }'::JSONB
+);
---
+++ dev/erc20/index.ts
@@ -0,0 +1,38 @@
+import { createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { foundry } from "viem/chains";
+
+import ERC20 from "./ERC20.json";
+
+const CONTRACT = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+const [ACCOUNT_1, ACCOUNT_2] = [
+  privateKeyToAccount(
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+  ),
+  privateKeyToAccount(
+    "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+  ),
+];
+
+const client = createWalletClient({
+  chain: foundry,
+  transport: http("http://pg-chainsync-foundry:8545"),
+});
+
+console.log("Transferring tokens every second!");
+
+// Transfer tokens every second
+setInterval(async () => {
+  await client.writeContract({
+    chain: foundry,
+    address: CONTRACT,
+    abi: ERC20.abi,
+    functionName: "transfer",
+    args: [
+      ACCOUNT_2.address,
+      `${Math.ceil(Math.random() * 1000)}` + "0".repeat(18),
+    ],
+    account: ACCOUNT_1,
+  });
+}, 3000);
