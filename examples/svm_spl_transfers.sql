d9da92527d822228864f6022b5e300220e86f56b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2025-05-24 04:02:24 +0200

    examples: handle mint/burn instructions

--- examples/svm_spl_transfers.sql
+++ examples/svm_spl_transfers.sql
@@ -22,15 +22,6 @@ BEGIN
 
   bytes := base58.decode(inst.data);
 
-  discriminator := get_byte(bytes, 0);
-  IF discriminator = 3 THEN
-    source := inst.accounts[1]; -- or accounts_owners[1]
-    destination := inst.accounts[2]; -- or accounts_owners[2]
-  ELSIF discriminator = 12 THEN
-    source := inst.accounts[1]; -- or accounts_owners[1]
-    destination := inst.accounts[3]; -- or accounts_owners[3]
-  END IF;
-
   SELECT
     get_byte(bytes, 1) +
     get_byte(bytes, 2) * 256 +
@@ -41,9 +32,27 @@ BEGIN
     get_byte(bytes, 7) * 256^6 +
     get_byte(bytes, 8) * 256^7
   INTO amount;
-
   amount := amount / POWER(10, (job->>'decimals')::SMALLINT);
-  RAISE LOG 'Transfer % -> % amount: %', source, destination, amount;
+
+  discriminator := get_byte(bytes, 0);
+  IF discriminator = 3 THEN -- Transfer
+    source := inst.accounts[1]; -- or accounts_owners[1]
+    destination := inst.accounts[2]; -- or accounts_owners[2]
+    RAISE LOG 'Transfer % -> % amount: %', source, destination, amount;
+  ELSIF discriminator = 12 THEN -- TransferChecked
+    source := inst.accounts[1]; -- or accounts_owners[1]
+    destination := inst.accounts[3]; -- or accounts_owners[3]
+    RAISE LOG 'Transfer % -> % amount: %', source, destination, amount;
+  ELSIF discriminator = 7 THEN -- MintTo
+    source := '';
+    destination := inst.accounts[2]; -- or accounts_owners[1]
+    RAISE LOG 'Mint % amount: %', destination, amount;
+  ELSIF discriminator = 8 THEN -- Burn
+    source := inst.accounts[1]; -- or accounts_owners[1]
+    destination := '';
+    RAISE LOG 'Burn % amount: %', source, amount;
+  END IF;
+
 
   INSERT INTO transfers (source, destination, amount, timestamp)
   VALUES (source, destination, amount, TO_TIMESTAMP(inst.block_time) AT TIME ZONE 'UTC')
@@ -63,7 +72,7 @@ SELECT chainsync.register(
     "program": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
     "mentions": ["<your_token>"],
     "instruction_handler": "svm_transfer_handler",
-    "instruction_discriminators": [3, 12],
+    "instruction_discriminators": [3, 12, 7, 8],
 
     "decimals": 6
   }'::JSONB
